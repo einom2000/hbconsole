d0451f63ae414f4e0b4acdb163cd383797119252
einom2000 <einom.ng@gmail.com>
2018-12-23 23:07:41 +0800

    111

--- console.py
+++ console.py
@@ -1,18 +1,22 @@
-# open D:\Program Files (x86)\Battle.net\Battle.net\Launcher.exe
-# log in battelnet
-# switch to hearthstone
-# remove file: D:\Program Files (x86)\Hearthstone\Hearthstone_Data\Mono\MonoPosixHelper.dll
-#              D:\Program Files (x86)\Hearthstone\Hearthstone_Data\Plugins\bnl_checkout_client.dll
-# run game
+# open bt shortcut on desktop
+# log in bt
+# switch to hs
+# check files and
+# remove file:
+# run
 # wait and enter the deck
-# run hb D:\hb\Hearthbuddy.exe
+# run hb D:\hb\ symlink
 # wait and click start
+# monitor the hb start button and the win times
+# monitor the hs window status
+
 
 import os, win32api
 import win32gui,sys
 import time
 import pyautogui
 import win32process as process
+
 
 
 class LoginWindow:
@@ -60,7 +64,7 @@ class LoginWindow:
         pyautogui.typewrite(self.userPwd, interval=0.25)
 
 
-battlenet = 'D:\Program Files (x86)\Battle.net\Battle.net Launcher.exe'
+bt_shortcut = desktop = os.path.expanduser("~/Desktop/")'D:\Program Files (x86)\Battle.net\Battle.net Launcher.exe'
 hearthstone = 'D:\Program Files (x86)\Hearthstone\Hearthstone_Data'
 loginbt = LoginWindow(battlenet, '暴雪战网登录', 'einom2000@163.com', '123NUNUchipi')
 
