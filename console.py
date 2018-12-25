6cb44209f342d64ff42ef03dd877893725154bc0
einom2000 <einom.ng@gmail.com>
2018-12-25 13:03:36 +0800

    bn log on done

--- console.py
+++ console.py
@@ -11,24 +11,28 @@
 # monitor the hs window status
 
 
-import os, win32api
+import os, win32api,random
 import win32gui,sys
 import time
 import pyautogui
 import win32com.client
 import win32process as process
-import winshell
-# from win32api import GetSystemMetrics
-#
-# SYS_WIDTH = GetSystemMetrics(0)
-# SYS_HEIGHT = GetSystemMetrics(1)
+import winshell, psutil
 
-def kill_process(process_name):
-    pass
+
+def kill_process(process_name, wd_name):
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == process_name:
+            proc.kill()
+            print('killed' + process_name)
+    while win32gui.FindWindow(None, wd_name):
+         print(wd_name + 'still exist')
+         pass
+    return
 
 class LoginWindow:
-    windowUpLeftX = 0
-    windowUpLeftY = 0
+
     windowHwnd = 0
 
     def __init__(self, programdir, windowname, username, userpwd):
@@ -36,7 +40,6 @@ class LoginWindow:
         self.windowName = windowname
         self.userName = username
         self.userPwd = userpwd
-        self.rect = ()
 
     def runbnet(self):
         exist = win32gui.FindWindow(None, self.windowName)
@@ -51,52 +54,66 @@ class LoginWindow:
                 continue
             else:
                 win32gui.MoveWindow(hwndbnt, 100, 100, 365, 541, True)
-                self.rect = win32gui.GetWindowRect(hwndbnt)
-                print(self.rect)
             break
         win32gui.SetForegroundWindow(hwndbnt)
         time.sleep(0.5)
-        return hwndbnt, self.rect[0], self.rect[1]
+        return hwndbnt
 
     def login(self):
-        battlenetAccoutWindowX = self.windowUpLeftX + int(200 / 365 * self.rect[2])
-        battlenetAccoutWindowY = self.windowUpLeftY + int(230 / 541 * self.rect[3])
-        pyautogui.moveTo(battlenetAccoutWindowX, battlenetAccoutWindowY, 0.8, pyautogui.easeInOutQuad)
-        print(pyautogui.position())
-        pyautogui.click()
-        t = time.time()
-        while time.time() - t <= 5:
-            pyautogui.press('backspace')
+        # to log in id
+        for i in range(4):
+            pyautogui.press('tab')
+            time.sleep(random.randint(3, 5) / 10)
+        # clear box
+        pyautogui.press('backspace')
+        time.sleep(random.randint(3, 5) / 10)
+        # change to english
+        pyautogui.press('shift')
+        time.sleep(random.randint(3, 5) / 10)
         win32api.LoadKeyboardLayout('00000409', 1)
-        time.sleep(0.5)
-        # if win32api.GetKeyboardLayout() == 134481924:
-        #     pyautogui.press('shift')
-        #     # print(win32api.GetKeyboardLayout())
-        #     time.sleep(0.25)
-        pyautogui.typewrite(self.userName, interval=0.25)
-        time.sleep(0.25)
+        time.sleep(random.randint(3, 5) / 10)
+        # typein
+        pyautogui.typewrite(self.userName, interval=(random.randint(15, 30) / 100))
+        time.sleep((random.randint(15, 30) / 100))
         pyautogui.press('tab')
-        pyautogui.typewrite(self.userPwd, interval=0.25)
-        btnhdl = win32gui.FindWindowEx(self.windowHwnd, 0, "Button", "登录")
-        return btnhdl
+        pyautogui.typewrite(self.userPwd, interval=(random.randint(15, 30) / 100))
+        for i in range(3):
+            pyautogui.press('tab')
+            time.sleep(random.randint(3, 5) / 10)
+        # log in
+        pyautogui.press('enter')
+        return
 
 
 hb_dir = 'D:\hb\\'
 bn_target = winshell.shortcut(os.path.join(winshell.desktop(), "暴雪战网.lnk")).path
 # hs_target = winshell.shortcut(os.path.join(winshell.desktop(), "Hearthstone.exe - 快捷方式.lnk")).path
 # hb_target = hb_dir + os.readlink(os.path.join(hb_dir, "Hearthbuddy.exe"))
-
 # hearthstone = 'D:\Program Files (x86)\Hearthstone\Hearthstone_Data'
 loginbt = LoginWindow(bn_target, '暴雪战网登录', 'einom2000@163.com', '123NUNUchipi')
 
-loginbt.runbnet()
-btwindowInfo = loginbt.findWindow()
-loginbt.windowHwnd = btwindowInfo[0]
-loginbt.windowUpLeftX = btwindowInfo[1]
-loginbt.windowUpLeftY = btwindowInfo[2]
-print(loginbt.windowHwnd, loginbt.windowUpLeftX, loginbt.windowUpLeftY)
+logged_in = False
+logging_time = time.time()
+while not logged_in:
+    loginbt.runbnet()
+    bn_hwnd = loginbt.findWindow()
+    loginbt.login()
+    # wait for the battle net window shows up
+    time_login = time.time()
+    while time.time() - time_login <= 30:
+        bt_window = win32gui.FindWindow(None, '暴雪战网')
+        if bt_window > 0:
+            logged_in = True
+            break
+    if not logged_in:
+        kill_process('Battle.net.exe', '暴雪战网登录')
+    if time.time() - logging_time >= 600:
+        # after 10 minutes failure, terminate program
+        sys.exit()
 
-print(loginbt.login())
+# logged in Battle net!!!
+
+
 
 
 
--- consolehb.py
+++ consolehb.py
@@ -1,11 +1,27 @@
-import  os, pyautogui,keyboard, win32gui,win32api
+import  os, pyautogui,keyboard, win32gui,win32api, winshell, psutil
 # tsks = os.popen('tasklist').readlines()
 # for tsk in tsks:
 #     if 'Battle.net' in tsk:
 #         print(tsk)
 #         print ("yes!")
 
-while True:
-    if keyboard.is_pressed('space'):
-        # print(pyautogui.position())
-        print(win32api.GetKeyboardLayout())
+# while True:
+#     if keyboard.is_pressed('space'):
+#         hwnd = win32gui.GetForegroundWindow()
+#         txt = win32gui.GetWindowText(hwnd)
+#         print(txt)
+
+def kill_process(process_name, wd_name):
+    for proc in psutil.process_iter():
+        print(proc)
+        # check whether the process name matches
+        if proc.name() == process_name:
+            proc.kill()
+    return
+
+bn_target = winshell.shortcut(os.path.join(winshell.desktop(), "暴雪战网.lnk")).path
+print(bn_target)
+bn = 'Battle.net.exe'
+print(bn)
+print("try to kill")
+kill_process(bn, '暴雪战网登录')
