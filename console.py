0fef965c19a5ac8460b3c29cdadc7c8a2d82f3e3
einom2000 <einom.ng@gmail.com>
2018-12-25 23:30:06 +0800

    111

--- console.py
+++ console.py
@@ -18,7 +18,7 @@ import pyautogui
 import win32com.client
 import win32process as process
 import winshell, psutil
-
+import cv2
 
 def kill_process(process_name, wd_name):
     for proc in psutil.process_iter():
@@ -87,8 +87,8 @@ hb_dir = 'D:\hb\\'
 bn_target = winshell.shortcut(os.path.join(winshell.desktop(), "暴雪战网.lnk")).path
 hs_target = winshell.shortcut(os.path.join(winshell.desktop(), "Hearthstone.exe - 快捷方式.lnk")).path
 hb_target = hb_dir + os.readlink(os.path.join(hb_dir, "Hearthbuddy.exe"))
-hearthstone = 'D:\Program Files (x86)\Hearthstone\Hearthstone_Data'
-loginbt = LoginWindow(bn_target, '暴雪战网登录', 'einom2000@163.com', '123NUNUchipi')
+# hearthstone = 'D:\Program Files (x86)\Hearthstone\Hearthstone_Data'
+loginbt = LoginWindow(bn_target, '暴雪战网登录', 'e', '123chipi')
 
 logged_in = False
 logging_time = time.time()
@@ -110,6 +110,67 @@ while not logged_in:
         sys.exit()
 
 # logged in Battle net!!!
+# move bn window to 0,0
+win32gui.SetForegroundWindow(bt_window)
+bt_rec = win32gui.GetWindowRect(bt_window)
+win32gui.MoveWindow(bt_window, 0, 0, bt_rec[2] - bt_rec[0], bt_rec[3] - bt_rec[1], 1)
+time.sleep(1)
+
+# looking for hs and click waiting for hs
+x, y = pyautogui.locateCenterOnScreen('hs.png', region=(0, 0, bt_rec[2], bt_rec[3]), grayscale=False, confidence=0.9)
+pyautogui.moveTo(x, y, 1,  pyautogui.easeInQuad)
+pyautogui.click(x, y)
+time.sleep(1)
+x, y = pyautogui.locateCenterOnScreen('login.png', region=(0, 0, bt_rec[2], bt_rec[3]), grayscale=False, confidence=0.9)
+pyautogui.moveTo(x, y, 1,  pyautogui.easeInQuad)
+pyautogui.click(x, y)
+# waiting for hs running
+hs_is_running = False
+while not hs_is_running:
+    hs_window = win32gui.FindWindow(None,'炉石传说')
+    if hs_window > 0:
+        hs_is_running = True
+time.sleep(3)
+win32gui.SetForegroundWindow(hs_window)
+hs_rec = win32gui.GetWindowRect(hs_window)
+win32gui.MoveWindow(hs_window, 800, 500, hs_rec[2] - hs_rec[0], hs_rec[3] - hs_rec[1], 1)
+
+# close bt window
+kill_process('Battle.net.exe', '暴雪战网')
+
+#lauching hb
+win32api.WinExec('Hearthbuddy.exe')
+while True:
+    config_window = win32gui.FindWindow(None, 'Configuration Window')
+    print(config_window)
+    if config_window > 0:
+        win32gui.SetForegroundWindow(config_window)
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(1)
+        break
+
+hb_is_running = False
+while not hb_is_running:
+    hb_window = win32gui.FindWindow(None, 'Hearthbuddy[0.3.1446.417] 学习交流,免费使用,严禁贩卖!')
+    if hb_window > 0:
+        hb_is_running = True
+time.sleep(2)
+hb_rec = win32gui.GetWindowRect(hb_window)
+win32gui.MoveWindow(hb_window, 0, 0, hb_rec[2] - hb_rec[0], hb_rec[3] - hb_rec[1], 1)
+
+#waiting and click start for hbuddy
+time.sleep(5)
+while True:
+    found_hb_start = pyautogui.locateCenterOnScreen('hb_start.png', region=(0, 0, hb_rec[2], hb_rec[3]))
+    if found_hb_start:
+        print(found_hb_start)
+        pyautogui.moveTo(found_hb_start)
+        time.sleep(2)
+        pyautogui.click()
+        break
+
+
 
 
 
--- consolehb.py
+++ consolehb.py
@@ -5,23 +5,23 @@ import  os, pyautogui,keyboard, win32gui,win32api, winshell, psutil
 #         print(tsk)
 #         print ("yes!")
 
-# while True:
-#     if keyboard.is_pressed('space'):
-#         hwnd = win32gui.GetForegroundWindow()
-#         txt = win32gui.GetWindowText(hwnd)
-#         print(txt)
+while True:
+    if keyboard.is_pressed('space'):
+        hwnd = win32gui.GetForegroundWindow()
+        txt = win32gui.GetWindowText(hwnd)
+        print(txt)
 
-def kill_process(process_name, wd_name):
-    for proc in psutil.process_iter():
-        print(proc)
-        # check whether the process name matches
-        if proc.name() == process_name:
-            proc.kill()
-    return
-
-bn_target = winshell.shortcut(os.path.join(winshell.desktop(), "暴雪战网.lnk")).path
-print(bn_target)
-bn = 'Battle.net.exe'
-print(bn)
-print("try to kill")
-kill_process(bn, '暴雪战网登录')
+# def kill_process(process_name, wd_name):
+#     for proc in psutil.process_iter():
+#         print(proc)
+#         # check whether the process name matches
+#         if proc.name() == process_name:
+#             proc.kill()
+#     return
+# #
+# bn_target = winshell.shortcut(os.path.join(winshell.desktop(), "暴雪战网.lnk")).path
+# print(bn_target)
+# bn = 'Battle.net.exe'
+# print(bn)
+# print("try to kill")
+# kill_process(bn, '暴雪战网登录')
