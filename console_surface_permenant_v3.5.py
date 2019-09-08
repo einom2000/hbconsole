bab20afade1672690fbd08334c65c4cb2ff66ad8
einom2000 <einom.ng@gmail.com>
2019-09-08 15:51:01 +0800

    1111

--- console_surface_permenant_v3.5.py
+++ console_surface_permenant_v3.5.py
@@ -372,6 +372,8 @@ def initialize_hs_window(hs_window):
             logging.warning('last game was lost! click to confirm!')
             break
 
+    # click away the mission panels
+    move_and_click((739 + re_x, 116 + re_y))
     # here could add picking deck order and game style and farming or mounting in future
     t = time.time()
     while time.time() - t <= 20:
@@ -530,13 +532,6 @@ def start_buddy_round():
 
 # reset buddy status
 def reset_status():
-    global buddy_status
-    # if it is a new player start mining, reset counter
-    if not buddy_status and correct_buddy(buddy_status):
-        click_hb_btn(buddy_btn_dict['stats_btn'])
-        correct_buddy(not buddy_status)
-        buddy_status = not buddy_status
-
     time.sleep(1)
     click_hb_btn(buddy_btn_dict['stats_reset_btn'])
     logging.info('status info reset!')
